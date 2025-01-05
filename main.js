bdf0148d2910f2c6b33cec85d5442c9e94d0cbe3
beiliang <80659710+264040@users.noreply.github.com>
2025-01-05 18:07:07 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,305 @@
+      const ipaCategories = {
+      	"long-vowels": [{
+      			"item": "[ iː ]",
+      			"mp_item": "./Articulation/key_iː.mp3"
+      		},
+      		{
+      			"item": "[ ɑː ]",
+      			"mp_item": "./Articulation/key_ɑː.mp3"
+      		},
+      		{
+      			"item": "[ ɔː ]",
+      			"mp_item": "./Articulation/key_ɔː.mp3"
+      		},
+      		{
+      			"item": "[ uː ]",
+      			"mp_item": "./Articulation/key_uː.mp3"
+      		},
+      		{
+      			"item": "[ ɜː ]",
+      			"mp_item": "./Articulation/key_ɜː.mp3"
+      		}
+      	],
+      	"short-vowels": [{
+      			"item": "[ ɪ ]",
+      			"mp_item": "./Articulation/key_ɪ.mp3"
+      		},
+      		{
+      			"item": "[ ə ]",
+      			"mp_item": "./Articulation/key_ə.mp3"
+      		},
+      		{
+      			"item": "[ ʌ ]",
+      			"mp_item": "./Articulation/key_ʌ.mp3"
+      		},
+      		{
+      			"item": "[ ɒ ]",
+      			"mp_item": "./Articulation/key_ɒ.mp3"
+      		},
+      		{
+      			"item": "[ ʊ ]",
+      			"mp_item": "./Articulation/key_ʊ.mp3"
+      		},
+      		{
+      			"item": "[ æ ]",
+      			"mp_item": "./Articulation/key_æ.mp3"
+      		},
+      		{
+      			"item": "[ e ]",
+      			"mp_item": "./Articulation/key_e.mp3"
+      		}
+      	],
+      	"diphthongs": [{
+      			"item": "[ eɪ ]",
+      			"mp_item": "./Articulation/key_eɪ.mp3"
+      		},
+      		{
+      			"item": "[ aɪ ]",
+      			"mp_item": "./Articulation/key_aɪ.mp3"
+      		},
+      		{
+      			"item": "[ ɔɪ ]",
+      			"mp_item": "./Articulation/key_ɔɪ.mp3"
+      		},
+      		{
+      			"item": "[ aʊ ]",
+      			"mp_item": "./Articulation/key_aʊ.mp3"
+      		},
+      		{
+      			"item": "[ əʊ ]",
+      			"mp_item": "./Articulation/key_əʊ.mp3"
+      		},
+      		{
+      			"item": "[ ɪə ]",
+      			"mp_item": "./Articulation/key_ɪə.mp3"
+      		},
+      		{
+      			"item": "[ eə ]",
+      			"mp_item": "./Articulation/key_eə.mp3"
+      		},
+      		{
+      			"item": "[ ʊə ]",
+      			"mp_item": "./Articulation/key_ʊə.mp3"
+      		}
+      	],
+      	"bcpoy": [{
+      			"item": "[ p ]",
+      			"mp_item": "./Articulation/key_p.mp3"
+      		},
+      		{
+      			"item": "[ b ]",
+      			"mp_item": "./Articulation/key_b.mp3"
+      		},
+      		{
+      			"item": "[ t ]",
+      			"mp_item": "./Articulation/key_t.mp3"
+      		},
+      		{
+      			"item": "[ d ]",
+      			"mp_item": "./Articulation/key_d.mp3"
+      		},
+      		{
+      			"item": "[ k ]",
+      			"mp_item": "./Articulation/key_k.mp3"
+      		},
+      		{
+      			"item": "[ g ]",
+      			"mp_item": "./Articulation/key_g.mp3"
+      		}
+      	],
+      	"capoy": [{
+      			"item": "[ f ]",
+      			"mp_item": "./Articulation/key_f.mp3"
+      		},
+      		{
+      			"item": "[ v ]",
+      			"mp_item": "./Articulation/key_v.mp3"
+      		},
+      		{
+      			"item": "[ s ]",
+      			"mp_item": "./Articulation/key_s.mp3"
+      		},
+      		{
+      			"item": "[ z ]",
+      			"mp_item": "./Articulation/key_z.mp3"
+      		},
+      		{
+      			"item": "[ θ ]",
+      			"mp_item": "./Articulation/key_θ.mp3"
+      		},
+      		{
+      			"item": "[ ð ]",
+      			"mp_item": "./Articulation/key_ð.mp3"
+      		},
+      		{
+      			"item": "[ ʃ ]",
+      			"mp_item": "./Articulation/key_ʃ.mp3"
+      		},
+      		{
+      			"item": "[ ʒ ]",
+      			"mp_item": "./Articulation/key_ʒ.mp3"
+      		},
+      		{
+      			"item": "[ h ]",
+      			"mp_item": "./Articulation/key_h.mp3"
+      		}
+      	],
+      	"sdcay": [{
+      			"item": "[ tʃ ]",
+      			"mp_item": "./Articulation/key_tʃ.mp3"
+      		},
+      		{
+      			"item": "[ dʒ ]",
+      			"mp_item": "./Articulation/key_dʒ.mp3"
+      		},
+      		{
+      			"item": "[ tr ]",
+      			"mp_item": "./Articulation/key_tr.mp3"
+      		},
+      		{
+      			"item": "[ dr ]",
+      			"mp_item": "./Articulation/key_dr.mp3"
+      		},
+      		{
+      			"item": "[ ts ]",
+      			"mp_item": "./Articulation/key_ts.mp3"
+      		},
+      		{
+      			"item": "[ dz ]",
+      			"mp_item": "./Articulation/key_dz.mp3"
+      		}
+      	],
+      	"bifuy": [{
+      			"item": "[ m ]",
+      			"mp_item": "./Articulation/key_m.mp3"
+      		},
+      		{
+      			"item": "[ n ]",
+      			"mp_item": "./Articulation/key_n.mp3"
+      		},
+      		{
+      			"item": "[ ŋ ]",
+      			"mp_item": "./Articulation/key_ŋ.mp3"
+      		}
+      	],
+      	"uebmy": [{
+      			"item": "[ l ]",
+      			"mp_item": "./Articulation/key_l.mp3"
+      		},
+      		{
+      			"item": "[ r ]",
+      			"mp_item": "./Articulation/key_r.mp3"
+      		}
+      	],
+      	"bjyry": [{
+      			"item": "[ j ]",
+      			"mp_item": "./Articulation/key_j.mp3"
+      		},
+      		{
+      			"item": "[ w ]",
+      			"mp_item": "./Articulation/key_w.mp3"
+      		}
+      	]
+      }
+
+
+
+
+
+
+
+
+
+
+      function showCategory(category) { // 切换选项卡
+
+      	const grid = document.getElementById('ipa-grid');
+      	grid.innerHTML = '';
+
+      	ipaCategories[category].forEach(symbolObj => {
+      		const itemDiv = document.createElement('div');
+      		itemDiv.classList.add('item');
+      		itemDiv.textContent = symbolObj.item;
+      		itemDiv.onclick = () => playAudio(symbolObj.mp_item);
+      		grid.appendChild(itemDiv);
+      	});
+
+      	document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+      	document.getElementById('tab-' + category).classList.add('active');
+      }
+
+      function playAudio(mp_item) { //  播放音频
+      	const audio = new Audio(mp_item);
+      	audio.play();
+      }
+
+      window.onload = function () {
+
+      	// 数据结构，存储每个分类的信息
+      	const tabData = [{
+      			id: 'tab-long-vowels',
+      			category: 'long-vowels',
+      			label: '长元音'
+      		},
+      		{
+      			id: 'tab-short-vowels',
+      			category: 'short-vowels',
+      			label: '短元音'
+      		},
+      		{
+      			id: 'tab-diphthongs',
+      			category: 'diphthongs',
+      			label: '双元音'
+      		},
+      		{
+      			id: 'tab-bcpoy',
+      			category: 'bcpoy',
+      			label: '爆破音'
+      		},
+      		{
+      			id: 'tab-capoy',
+      			category: 'capoy',
+      			label: '擦破音'
+      		},
+      		{
+      			id: 'tab-sdcay',
+      			category: 'sdcay',
+      			label: '塞擦音'
+      		},
+      		{
+      			id: 'tab-bifuy',
+      			category: 'bifuy',
+      			label: '鼻辅音'
+      		},
+      		{
+      			id: 'tab-uebmy',
+      			category: 'uebmy',
+      			label: '舌边音'
+      		},
+      		{
+      			id: 'tab-bjyry',
+      			category: 'bjyry',
+      			label: '半元音'
+      		},
+      	];
+
+      	const tabsContainer = document.getElementById('tabs-container');
+
+      	// 动态生成tabs
+      	tabData.forEach(({
+      		category,
+      		label,
+      		id
+      	}) => {
+      		const tab = document.createElement('div');
+      		tab.classList.add('tab');
+      		tab.id = id;
+      		tab.textContent = label;
+      		tab.addEventListener('click', () => showCategory(category));
+
+      		tabsContainer.appendChild(tab);
+      	});
+
+      	// 显示选择的分类
+      	showCategory('long-vowels'); // 页面初始化显示// 
+      }
